05ca93f224af7389a5fac3e94220acad32f736a7
Arpit sachan <arpitsachan.as2001@gmail.com>
2024-01-29 19:03:36 +0530

    marksheet creator PDF generation

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 window.onload = () => {
+    window.jsPDF = window.jspdf.jsPDF;
+    var doc = jsPDF();
     // adding subject field
     var addSubjectBtn = document.getElementById("subjects");
     addSubjectBtn.onclick = () => {
@@ -126,22 +128,22 @@ window.onload = () => {
 
 
     // preview student image and upload
-    var studentPic = document.getElementById("studentPic");
+   /* var studentPic = document.getElementById("studentPic");
     studentPic.onchange = () => {
         var file = this.files[0];
         var url = URL.createObjectURL(file);
         var studentPicPreview = document.getElementById("student-pic-preview");
         studentPicPreview.src = url;
-    }
+    }*/
 
     // school logo picker
-    var schoolLogo = document.getElementById("schoolLogo");
+    /*var schoolLogo = document.getElementById("schoolLogo");
     schoolLogo.onchange = () => {
          var file = this.files[0];
          var url = URL.createObjectURL(file);
          var schoolLogoPreview = document.getElementById("schoolLogoPreview");
          schoolLogoPreview.src = url;
-}
+}*/
 
 
     // School Name Input 
@@ -200,15 +202,93 @@ window.onload = () => {
         rollInputPreview.innerHTML = this.value;
     }
 
-    //Export to PDF
-    var generate = document.getElementById("generate");
+    //Export to PDF one way
+    /*var generate = document.getElementById("generate");
     generate.onclick = function() {
         var preview = document.getElementById("preview");
         html2canvas(preview).then(function(result){
             var base64String = result.toDataURL();
-             var doc = new jsPDF('p','mm');
-             doc.addImage(base64String, 'PNG' , 10, 10);
+             var doc = jsPDF('p','mm');
+             doc.addImage(base64String, 'PNG' , 30, 30);
              doc.save('Marksheet.pdf');
         })
+    } */
+
+
+    //Getting text width
+    var findTextWidth = function(text , fontSize) {
+        var textWidth = doc.getTextDimensions(text,{
+            fontSize: fontSize
+        }).w;
+        return textWidth;
+    }
+
+    function getURL(file) {
+        var url = URL.createObjectURL(file);
+        return url;
+    }
+
+    var form = document.getElementById("marksheet-form");
+    form.onsubmit = function(event) {
+        event.preventDefault();
+        var elements = form.elements;
+        var logo = elements.schoolLogo.files[0];
+        var schoolUrl = URL.createObjectURL(logo);
+        var schoolName = elements.schoolName.value;
+        var tagLine = elements.tagLine.value;
+        var candidateName = elements.candidateName.value;
+        var fatherName = elements.fatherName.value;
+        var dob = elements.dob.value;
+        var gender = elements.gender.value;
+        var standard = elements.class.value;
+        var roll = elements.roll.value;
+
+        //Generate PDF
+        // live preview of logo and profile code is commented
+        var logoWidth = 30;
+        var pageWidth = doc.internal.pageSize.width;
+        var schoolLogoLeftMargin = (pageWidth-logoWidth)/2;
+        doc.addImage(schoolUrl, 'PNG', schoolLogoLeftMargin,10,logoWidth,30);
+        
+
+        var schoolNameFontSize = 28;
+        var schooNameWidth = findTextWidth(schoolName , schoolNameFontSize);
+        var schoolNameLeftMargin = (pageWidth-schooNameWidth)/2;
+        doc.setFontSize(schoolNameFontSize);
+        doc.text(schoolName, schoolNameLeftMargin,45);
+
+
+        var tagLineFontSize = 14;
+        var tagLineWidth = findTextWidth(tagLine, tagLineFontSize);
+        var tagLineLeftMargin = (pageWidth-tagLineWidth)/2;
+        doc.setFontSize(tagLineFontSize);
+        doc.text(tagLine, tagLineLeftMargin, 55);
+
+        doc.autoTable(
+            {
+                head: [['subjects', 'Fullmarks', 'Obtained Marks']],
+                body: [
+                        ['Maths', '100', '80'],
+                        ['English', '100', '78']
+                      ]
+            }
+        )
+
+
+
+
+        doc.save("marksheet.pdf");
     }
 }
+
+
+/*
+
+1. doc.text()
+2. doc.save()
+3. doc.setFontSize()
+4. doc.setFont()
+5. doc.addImage('path' , 'jpeg' , leftMargin , topMargin , imagewidth , imageheight)
+6. doc.internal.pageSize.width (to view pdf width)
+
+*/
